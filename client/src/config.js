1f8db9aa9f7329258aaf8ad56c444b2488e713c1
ajinkya252005 <158799085+ajinkya252005@users.noreply.github.com>
2026-03-20 19:21:48 +0530

    switched to production link

---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+// 🟢 Uncomment the line below when running locally:
+//export const API_BASE_URL = "http://localhost:5000";
+
+// 🔴 Uncomment the line below when pushing to production (Render):
+export const API_BASE_URL = "https://cricket-api-ll8u.onrender.com";
